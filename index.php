f634410b848674d1234d6d60bc44c0f9f6710fbb
akayiekyaw <akarikyawucsmonywa@gmail.com>
2024-09-03 19:51:11 +0630

    Upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+echo"Hello World!";
+echo"<h1>Super Friday.</h1>";
+$language_type = "Server-side programming language";
+echo $language_type;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "<h1>This is paragraph"
+    ?>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia nihil debitis saepe aut provident sit, asperiores corrupti, voluptates perferendis laboriosam labore consequatur pariatur, temporibus deserunt quis! Iure sapiente dignissimos consequuntur.</p>
+
+<script>
+    document.write('<h1>I am Java Script</h1>');
+    </script>
+
+</body>
+</html>
---
+++ variable_and_string.php
@@ -0,0 +1,20 @@
+<?php
+//Variable
+//Naming Rule - No Space, no special Characters!@#$%^&*(), Can't be a number on first character, Case Sensitive
+
+$name ="Hein Min Htet";
+$address = "Mandalay";
+
+
+//double quote and single quote
+
+echo "My name is $name";
+echo '<br>';
+echo "I live in $address";//variable " "
+echo 'I live in $address<br>';
+echo 'I live in '.$address;
+echo '<br>';
+echo $name.'Live in'.$address;
+
+
+?>
